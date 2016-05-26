1f08251ab1aaa217787e3c6c0e0c26eabf7b27c7
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2016-05-26 16:39:33 +0200

    Introduce ISpoofaxResult class across all commands.

--- org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/client/ReplModule.java
+++ org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/client/ReplModule.java
@@ -21,6 +21,7 @@ import org.metaborg.spoofax.shell.hooks.OnEvalSuccessHook;
 import org.metaborg.spoofax.shell.invoker.ICommandFactory;
 import org.metaborg.spoofax.shell.invoker.ICommandInvoker;
 import org.metaborg.spoofax.shell.invoker.SpoofaxCommandInvoker;
+import org.metaborg.spoofax.shell.output.IResultFactory;
 
 import com.google.common.io.Files;
 import com.google.inject.Provides;
@@ -70,6 +71,7 @@ public class ReplModule extends SpoofaxModule {
         // @formatter:on
 
         install(new FactoryModuleBuilder().build(ICommandFactory.class));
+        install(new FactoryModuleBuilder().build(IResultFactory.class));
     }
 
     /**
