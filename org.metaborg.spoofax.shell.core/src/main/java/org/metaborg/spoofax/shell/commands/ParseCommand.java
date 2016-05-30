2adb296d91f1e25ea55b25e9f1df843b2c6f44c4
Jente Hidskes <hjdskes@gmail.com>
2016-05-30 16:38:14 +0200

    Move IEditor, IDisplay and IInputHistory back to core
    The Eclipse implementation turned out to be similar, so the interfaces are
    useful to have in core.

--- org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/commands/ParseCommand.java
+++ org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/commands/ParseCommand.java
@@ -11,7 +11,7 @@ import org.metaborg.core.project.IProject;
 import org.metaborg.spoofax.core.syntax.ISpoofaxSyntaxService;
 import org.metaborg.spoofax.core.syntax.SpoofaxSyntaxService;
 import org.metaborg.spoofax.core.unit.ISpoofaxParseUnit;
-import org.metaborg.spoofax.shell.hooks.IResultHook;
+import org.metaborg.spoofax.shell.client.hooks.IResultHook;
 import org.metaborg.spoofax.shell.output.IResultFactory;
 import org.metaborg.spoofax.shell.output.InputResult;
 import org.metaborg.spoofax.shell.output.ParseResult;
