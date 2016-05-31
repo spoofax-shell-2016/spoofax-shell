1234f3010dbd217ca27426d03644e776c6ec6f80
Gerlof Fokkema <gerlof.fokkema@gmail.com>
2016-05-31 22:20:47 +0200

    Change styledtext to store regions and text apart.

--- org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/output/StyledText.java
+++ org.metaborg.spoofax.shell.core/src/main/java/org/metaborg/spoofax/shell/output/StyledText.java
@@ -2,15 +2,15 @@ package org.metaborg.spoofax.shell.output;
 
 import java.awt.Color;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.metaborg.core.source.ISourceRegion;
+import org.metaborg.core.source.SourceRegion;
 import org.metaborg.core.style.IRegionStyle;
 import org.metaborg.core.style.IStyle;
 import org.metaborg.core.style.RegionStyle;
 import org.metaborg.core.style.Style;
-import org.spoofax.terms.StrategoString;
+import org.spoofax.interpreter.terms.IStrategoTerm;
 
 import com.google.common.collect.Lists;
 
@@ -19,7 +19,8 @@ import com.google.common.collect.Lists;
  * {@link IRegionStyle}.
  */
 public class StyledText {
-    private final List<IRegionStyle<String>> source;
+    private final List<IRegionStyle<IStrategoTerm>> source;
+    private StringBuffer textBuffer;
 
     /**
      * Create a styled text from a string with no style.
@@ -28,8 +29,7 @@ public class StyledText {
      *            The unstyled text.
      */
     public StyledText(String text) {
-        this.source = Lists.newArrayList();
-        this.append(text);
+        this(new Style(null, null, false, false, false), text);
     }
 
     /**
@@ -41,8 +41,7 @@ public class StyledText {
      *            The unstyled text.
      */
     public StyledText(Color color, String text) {
-        this.source = Lists.newArrayList();
-        this.append(color, text);
+        this(new Style(color, null, false, false, false), text);
     }
 
     /**
@@ -64,9 +63,8 @@ public class StyledText {
      * @param sourceRegions
      *            The list of styled Stratego terms.
      */
-    public StyledText(Iterable<IRegionStyle<StrategoString>> sourceRegions) {
-        this.source = Lists.newArrayList();
-        sourceRegions.forEach(e -> this.append(e.region(), e.style(), e.fragment().stringValue()));
+    public StyledText(Iterable<IRegionStyle<IStrategoTerm>> sourceRegions) {
+        this.source = Lists.newArrayList(sourceRegions);
     }
 
     /**
@@ -74,8 +72,46 @@ public class StyledText {
      *
      * @return All the styled strings in this text.
      */
-    public List<IRegionStyle<String>> getSource() {
+    public List<IRegionStyle<IStrategoTerm>> getSource() {
         return source;
+    }
+
+    /**
+     * Append a string with no style to this styled text.
+     *
+     * @param text
+     *            The unstyled text.
+     * @return The styled text.
+     */
+    public StyledText append(String text) {
+        return this.append(defaultStyle(), text);
+    }
+
+    /**
+     * Append a colored string to this styled text.
+     *
+     * @param color
+     *            The color to apply.
+     * @param text
+     *            The unstyled text.
+     * @return The styled text.
+     */
+    public StyledText append(Color color, String text) {
+        return this.append(colorStyle(color, null), text);
+    }
+
+    /**
+     * Append a string with an arbitrary style to this styled text.
+     *
+     * @param style
+     *            The style to apply.
+     * @param text
+     *            The unstyled text.
+     * @return The styled text.
+     */
+    public StyledText append(IStyle style, String text) {
+        int start = this.textBuffer.length() - 1;
+        return this.append(new SourceRegion(start, start + text.length() - 1), style, text);
     }
 
     /**
@@ -89,46 +125,18 @@ public class StyledText {
      *            The unstyled text.
      * @return The styled text.
      */
-    public StyledText append(ISourceRegion region, IStyle style, String text) {
-        this.source.add(new RegionStyle<String>(region, style, text));
+    private StyledText append(ISourceRegion region, IStyle style, String text) {
+        this.source.add(new RegionStyle<>(region, style, null));
+        this.textBuffer.append(text);
         return this;
     }
 
-    /**
-     * Append a string with no style to this styled text.
-     *
-     * @param text
-     *            The unstyled text.
-     * @return The styled text.
-     */
-    public StyledText append(String text) {
-        return this.append(null, null, text);
+    private IStyle defaultStyle() {
+        return colorStyle(null, null);
     }
 
-    /**
-     * Append a colored string to this styled text.
-     *
-     * @param color
-     *            The color to apply.
-     * @param text
-     *            The unstyled text.
-     * @return The styled text.
-     */
-    public StyledText append(Color color, String text) {
-        return this.append(null, new Style(color, null, false, false, false), text);
-    }
-
-    /**
-     * Append a string with an arbitrary style to this styled text.
-     *
-     * @param style
-     *            The style to apply.
-     * @param text
-     *            The unstyled text.
-     * @return The styled text.
-     */
-    public StyledText append(IStyle style, String text) {
-        return this.append(null, style, text);
+    private IStyle colorStyle(Color fg, Color bg) {
+        return new Style(fg, bg, false, false, false);
     }
 
     @Override
@@ -144,8 +152,8 @@ public class StyledText {
 
         return IntStream.range(0, source.size())
             .mapToObj(e -> {
-                IRegionStyle<String> s = source.get(e);
-                IRegionStyle<String> o = other.source.get(e);
+                IRegionStyle<IStrategoTerm> s = source.get(e);
+                IRegionStyle<IStrategoTerm> o = other.source.get(e);
 
                 return s.fragment().equals(o.fragment())
                         && (s.style() == o.style()
@@ -164,6 +172,6 @@ public class StyledText {
 
     @Override
     public String toString() {
-        return source.stream().map(e -> e.fragment()).collect(Collectors.joining());
+        return textBuffer.toString();
     }
 }
