c0135e4fbafe53298ae3e8f243279de3848bf2e8
Jente Hidskes <hjdskes@gmail.com>
2016-06-29 20:01:10 +0200

    Eclipse: replace custom isFile check by builtin check

--- org.metaborg.spoofax.shell.eclipse/src/main/java/org/metaborg/spoofax/shell/client/eclipse/properties/ReplPropertyTester.java
+++ org.metaborg.spoofax.shell.eclipse/src/main/java/org/metaborg/spoofax/shell/client/eclipse/properties/ReplPropertyTester.java
@@ -1,7 +1,6 @@
 package org.metaborg.spoofax.shell.client.eclipse.properties;
 
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchWindow;
@@ -10,26 +9,19 @@ import org.metaborg.spoofax.shell.client.eclipse.ReplView;
 
 /**
  * A {@link PropertyTester} to test properties relevant for the Spoofax Shell
- * REPL. It can test whether the current selection is a file ({@code "isFile"})
- * and whether there is a REPL open ({@code "isReplOpen"}).
+ * REPL. It can test whether there is a REPL open (property {@code "isReplOpen"}).
  */
 public class ReplPropertyTester extends PropertyTester {
 
     @Override
     public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
         switch (property) {
-        case "isFile":
-            return isFile(receiver);
         case "isReplOpen":
             return isReplOpen();
         default:
         }
 
         return false;
-    }
-
-    private boolean isFile(Object receiver) {
-        return receiver instanceof IResource && ((IResource) receiver).getType() == IResource.FILE;
     }
 
     private boolean isReplOpen() {
